d94499d99e4ecce89e2fd0b1a502a5c90c634611
MohamedAzharutheen <121545171+MohamedAzharutheen@users.noreply.github.com>
2024-11-20 15:57:18 +0530

    Update next.config.js
    Chnage nextconfig

--- client/next.config.js
+++ client/next.config.js
@@ -49,7 +49,7 @@ const nextConfig = {
   swcMinify: true,
   productionBrowserSourceMaps: true,
   env: {
-    url: prod ? "https://adh-theen-source.vercel.app" : "http://localhost:5000",
+    url: prod ? "http://localhost:5000" : "https://adh-theen-source.vercel.app",
     dev: !prod,
     phoneDis: "+91 950077 1134",
     phoneNum: "+91 9500771134",
